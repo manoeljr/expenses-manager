0f578d50090bdf141d8d1d71e1b372b7dfa0f3eb
Manoel <manoel.junior@tceal.tc.br>
2024-05-22 22:31:36 -0300

    feat: Adicionando componente list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './feature/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ListComponent }
+];
